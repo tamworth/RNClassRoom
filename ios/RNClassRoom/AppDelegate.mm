f321a6ee52412d054e29580e6a630d8034076bef
sheen <tamworth@163.com>
2022-05-04 18:23:06 +0800

    enable unbundle common & room (debug & release works)

--- ios/RNClassRoom/AppDelegate.mm
+++ ios/RNClassRoom/AppDelegate.mm
@@ -5,6 +5,8 @@
 #import <React/RCTRootView.h>
 
 #import <React/RCTAppSetupUtils.h>
+#import "ScriptLoadUtil.h"
+#import "ReactController.h""
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -16,6 +18,7 @@
 
 #import <react/config/ReactNativeConfig.h>
 
+
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
   RCTTurboModuleManager *_turboModuleManager;
   RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
@@ -25,36 +28,77 @@
 @end
 #endif
 
+@interface AppDelegate ()
+{
+  RCTBridge *bridge;
+}
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTAppSetupPrepareApp(application);
+//  RCTAppSetupPrepareApp(application);
 
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+//  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
-#if RCT_NEW_ARCH_ENABLED
-  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
-  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
-  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
-  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
-  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
-#endif
+//#if RCT_NEW_ARCH_ENABLED
+//  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
+//  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
+//  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
+//  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
+//  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
+//#endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"RNClassRoom", nil);
+//  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"RNClassRoom", nil);
 
-  if (@available(iOS 13.0, *)) {
-    rootView.backgroundColor = [UIColor systemBackgroundColor];
-  } else {
-    rootView.backgroundColor = [UIColor whiteColor];
-  }
+//  if (@available(iOS 13.0, *)) {
+//    rootView.backgroundColor = [UIColor systemBackgroundColor];
+//  } else {
+//    rootView.backgroundColor = [UIColor whiteColor];
+//  }
+  BOOL debugable = [ScriptLoadUtil isDebugable];
+  NSURL *jsCodeLocation;
+//  if(debugable){
+//    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"MultiDenugEntry" fallbackResource:nil];
+//  }else{
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"platform.ios" withExtension:@"bundle"];
+//  }
 
+  bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation
+                                 moduleProvider:nil
+                                  launchOptions:launchOptions];
+  [ScriptLoadUtil init:bridge];
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
+//  rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  dispatch_after(1, dispatch_get_main_queue(), ^{
+    UIViewController* vc = [self getRNVC];
+    [rootViewController presentViewController:vc animated:YES completion:nil];
+  });
   return YES;
+}
+
+
+- (UIViewController*)getRNVC {
+  BOOL isBundleLoaded = NO;
+  if([ScriptLoadUtil isDebugable]){
+    isBundleLoaded = YES;
+  }
+  NSString* bundleName = @"index.ios.bundle";
+  BundleType type = InApp;
+
+  NSString* bundleUrl = @"";
+  NSString* moduleName = @"RNClassRoom";
+  ReactController* controller = [[ReactController alloc] initWithURL:bundleUrl
+                                                                path:bundleName
+                                                                type:type
+                                                          moduleName:moduleName];
+  
+  return controller;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
