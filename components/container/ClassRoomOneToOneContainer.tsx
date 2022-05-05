289b19266cbccc51ac2ee8a65a851ea480587259
sheen <tamworth@163.com>
2022-05-05 23:43:59 +0800

    enable load remote dynamic component

--- components/container/ClassRoomOneToOneContainer.tsx
+++ components/container/ClassRoomOneToOneContainer.tsx
@@ -6,12 +6,13 @@ import {
 } from 'react-native';
 import RCTRenderView from '../widgets/RCTRenderView';
 import BaseClassRoomContainer from './BaseClassRoomContainer';
+import DynamicComponent from '../dynamic/dynamic-component';
 
 class ClassRoomOneToOneContainer extends BaseClassRoomContainer {
 
-    // render(): JSX.Element {
-    //     return <View />
-    // }
+    render(): JSX.Element {
+        return <DynamicComponent __id="counter" />
+    }
 }
 
 
